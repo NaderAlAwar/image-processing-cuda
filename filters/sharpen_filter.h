97ea268734f2eaf2894bf713980e86b7c9823068
Nader Al Awar <naderalawar@gmail.com>
2020-04-25 17:10:24 -0500

    updated main

--- filters/sharpen_filter.h
+++ filters/sharpen_filter.h
@@ -5,17 +5,17 @@
 #include "util.h"
 
 stbi_uc* sharpen(stbi_uc* input_image, int width, int height, int channels);
-__global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, int height, int channels, int total_threads, int padded_width, int padded_height, stbi_uc* mask, int mask_size);
+__global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, int height, int channels, int total_threads, int padded_width, int padded_height, int* mask, int mask_size);
 
 stbi_uc* sharpen(stbi_uc* input_image, int width, int height, int channels) {
-    stbi_uc mask[] = {126, 126, 126,
-                      126, 135, 126,
-                      126, 126, 126};
+    int mask[] = {0, -1, 0,
+                      -1, 5, -1,
+                      0, -1, 0};
     int mask_size = 3;
     int* d_mask;
 
-    cudaMallocManaged(&d_mask, mask_size * mask_size * sizeof(stbi_uc));
-    cudaMemcpy(d_mask, mask, mask_size * mask_size * sizeof(stbi_uc), cudaMemcpyHostToDevice);
+    cudaMallocManaged(&d_mask, mask_size * mask_size * sizeof(int));
+    cudaMemcpy(d_mask, mask, mask_size * mask_size * sizeof(int), cudaMemcpyHostToDevice);
 
     int padded_width = width;
     int padded_height = height;
@@ -36,14 +36,14 @@ stbi_uc* sharpen(stbi_uc* input_image, int width, int height, int channels) {
     int blocks = (threads == total_threads) ? 1 : total_threads / MAX_THREADS;
 
     printf("Blocks %d, threads %d\n", blocks, threads);
-    sharpen<<<blocks, threads>>>(d_input_image, d_output_image, width, height, channels, padded_width, padded_height, total_threads, d_mask, mask_size);
+    sharpen<<<blocks, threads>>>(d_input_image, d_output_image, width, height, channels, total_threads, padded_width, padded_height, d_mask, mask_size);
     cudaDeviceSynchronize();
 
     cudaMemcpy(h_output_image, d_output_image, image_size, cudaMemcpyDeviceToHost);
     return h_output_image;
 }
 
-__global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, int height, int channels, int padded_width, int padded_height, int total_threads, stbi_uc* mask, int mask_size) {
+__global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, int height, int channels, int total_threads, int padded_width, int padded_height, int* mask, int mask_size) {
     const int thread_id = threadIdx.x + blockDim.x * blockIdx.x;
     if (thread_id >= total_threads) {
         return;
@@ -57,9 +57,10 @@ __global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, 
     getPixel(input_image, padded_width, x_coordinate, y_coordinate, &pixel);
 
     Pixel current_pixel;
-    stbi_uc red = 0;
-    stbi_uc blue = 0;
-    stbi_uc green = 0;
+    int red = 0;
+    int blue = 0;
+    int green = 0;
+    int alpha = 0;
     for (int i = 0; i < mask_size; i++) {
         for (int j = 0; j < mask_size; j++) {
             getPixel(input_image, padded_width, x_coordinate - padding_size + i, y_coordinate - padding_size + j, &current_pixel);
@@ -68,12 +69,33 @@ __global__ void sharpen(stbi_uc* input_image, stbi_uc* output_image, int width, 
             red += current_pixel.r * mask_element;
             green += current_pixel.g * mask_element;
             blue += current_pixel.b * mask_element;
+            alpha += current_pixel.a * mask_element;
         }
     }
 
-    pixel.r = (stbi_uc) red;
-    pixel.g = (stbi_uc) green;
-    pixel.b = (stbi_uc) blue;
+    if (red < 0) {
+        pixel.r = 0;
+    } else if (red > 255) {
+        pixel.r = 255;
+    } else {
+        pixel.r = red;
+    }
+    if (green < 0) {
+        pixel.g = 0;
+    } else if (green > 255) {
+        pixel.g = 255;
+    } else {
+        pixel.g = green;
+    }
+    if (blue < 0) {
+        pixel.b = 0;
+    } else if (red > 255) {
+        pixel.b = 255;
+    } else {
+        pixel.b = red;
+    }
+
+    setPixel(output_image, padded_width, x_coordinate, y_coordinate, &pixel);
 }
 
 #endif
