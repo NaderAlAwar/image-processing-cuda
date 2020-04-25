e9586ad1faac60fe9450c2e81941069e49069a32
Nader Al Awar <naderalawar@gmail.com>
2020-04-24 20:35:09 -0500

    updated sharpen

--- filters/util.h
+++ filters/util.h
@@ -5,33 +5,33 @@
 
 const int MAX_THREADS = 1024;
 
-stbi_uc* zeroPadImage(stbi_uc* input_image, int &width, int &height, int channels, int filterSize);
+stbi_uc* zeroPadImage(stbi_uc* input_image, int &width, int &height, int channels, int filter_size);
 
-stbi_uc* zeroPadImage(stbi_uc* input_image, int &width, int &height, int channels, int filterSize) {
-    int halfFilterSize = filterSize / 2;
-    int paddedWidth = width + 2 * halfFilterSize;
-    int paddedHeight = height + 2 * halfFilterSize;
+stbi_uc* zeroPadImage(stbi_uc* input_image, int &width, int &height, int channels, int filter_size) {
+    int half_filter_size = filter_size / 2;
+    int padded_width = width + 2 * half_filter_size;
+    int padded_height = height + 2 * half_filter_size;
 
-    stbi_uc* paddedImage = (stbi_uc*) malloc(channels * paddedWidth * paddedHeight * sizeof(stbi_uc));
+    stbi_uc* padded_image = (stbi_uc*) malloc(channels * padded_width * padded_height * sizeof(stbi_uc));
 
-    Pixel zeroPixel = { .r = 0, .g = 0, .b = 0, .a = 0 };
-    Pixel otherPixel;
+    Pixel zero_pixel = { .r = 0, .g = 0, .b = 0, .a = 0 };
+    Pixel other_pixel;
 
-    for (int i = 0; i < paddedWidth; i++) {
-        for (int j = 0; j < paddedHeight; j++) {
-            if (i < halfFilterSize || i > paddedWidth - halfFilterSize || j < halfFilterSize || j > paddedWidth - halfFilterSize) {
-                setPixel(paddedImage, paddedWidth, i, j, &zeroPixel);
+    for (int i = 0; i < padded_width; i++) {
+        for (int j = 0; j < padded_height; j++) {
+            if (i < half_filter_size || i > padded_width - half_filter_size || j < half_filter_size || j > padded_width - half_filter_size) {
+                setPixel(padded_image, padded_width, i, j, &zero_pixel);
             } else {
-                getPixel(input_image, width, i - halfFilterSize, j - halfFilterSize, &otherPixel);
-                setPixel(paddedImage, paddedWidth, i, j, &otherPixel);
+                getPixel(input_image, width, i - half_filter_size, j - half_filter_size, &other_pixel);
+                setPixel(padded_image, padded_width, i, j, &other_pixel);
             }
         }
     }
 
-    width = paddedWidth;
-    height = paddedHeight;
+    width = padded_width;
+    height = padded_height;
 
-    return paddedImage;
+    return padded_image;
 }
 
 #endif
