24ba675133c007f81867532846e59eede82b1872
Nader Al Awar <naderalawar@gmail.com>
2020-04-30 16:08:11 -0500

    updated ex_batch

--- ex_batch.sh
+++ ex_batch.sh
@@ -1,5 +1,6 @@
 nvcc main.cu
-unzip images/batch.zip -d images/batch
+mkdir -p images/batch
+for i in {1..2000}; do cp images/lena_rgb.png "images/batch/$i.png"; done
 
 mkdir -p expected_output/batch
 ./a.out images/batch expected_output/batch edge batch 
